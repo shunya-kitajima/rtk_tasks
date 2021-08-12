3a658cb59e0893dd354d112cc62f75aa24516cb8
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-08-12 14:58:36 +0900

    必要なコンポーネントをインポート

--- src/features/login/Login.js
+++ src/features/login/Login.js
@@ -1,6 +1,24 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import { useSelector, useDispatch } from "react-redux";
+
+import styles from "./Login.module.css";
+import {
+  editUsername,
+  editPassword,
+  toggleMode,
+  fetchAsyncLogin,
+  fetchAsyncRegister,
+  selectAuthen,
+  selectIsLoginView,
+} from "./loginSlice";
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const authen = useSelector(selectAuthen);
+  const isLoginView = useSelector(selectIsLoginView);
+  const btnDisabler = authen.username === "" || authen.password === "";
+
   return <div>Hello Login</div>;
 };
 
